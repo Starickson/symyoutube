3f7a981f8cf1364786e54d72cd292526c5b44212
Eric <eric.mayemba@gmail.com>
2021-01-30 16:51:16 +0100

    ajout du controller YoutubeController et du YoutubeType

---
+++ src/Controller/YoutubeController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Youtube;
+use App\Form\YoutubeType;
+use App\Repository\YoutubeRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class YoutubeController extends AbstractController
+{
+    /**
+     * @Route("/", name="app-home")
+     */
+    public function index(Request $request,EntityManagerInterface $entityManager,YoutubeRepository $youtuberepository): Response
+    {
+        $youtube=new Youtube();
+        $form=$this->createForm(YoutubeType::class,$youtube);
+        $form->handleRequest($request);
+        if($form->isSubmitted() && $form->isValid()){
+            $entityManager->persist($youtube);
+            $entityManager->flush();
+            return $this->redirectToRoute('app-home');
+        }
+        return $this->render('youtube/index.html.twig', [
+            'form' =>$form->createView(),
+            'youtubes'=>$youtuberepository->findAll()
+        ]);
+    }
+}
